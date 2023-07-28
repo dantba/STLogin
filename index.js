0a0b5520fbdca20220503789e8ba54f9494b1b19
Daniel Araújo <daniel.araujo@buser.com.br>
2023-07-27 23:02:42 -0300

    @000 feat - login automatico no ST

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("hello")
+require('dotenv').config()
+const puppeteer = require('puppeteer');
+
+async function stRobot() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    const stUrl = 'https://sociotorcedor.com.br/';
+    await page.goto(stUrl);
+    const [a] = await page.$x("//a[contains(., 'LOGIN')]");
+    if (a) {
+        await a.click();
+    }
+    await page.type("[type='password']", process.env.ST_PASSWORD)
+    await page.type("[type='text']", process.env.ST_LOGIN)
+    await page.click("[type='submit']")
+
+
+
+}
+
+stRobot();
